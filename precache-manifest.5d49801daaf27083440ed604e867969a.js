3b4690d75704fa851367fa4e420a453209ee7dcf
imranstd <imransyd.88@gmail.com>
2019-03-22 14:37:35 +0100

    Updates

---
+++ precache-manifest.5d49801daaf27083440ed604e867969a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b2d99a3185091f2924",
+    "url": "/kaustictest/static/js/runtime~main.fb0b4e31.js"
+  },
+  {
+    "revision": "5c67d2505568381bd155",
+    "url": "/kaustictest/static/js/main.41ca69ef.chunk.js"
+  },
+  {
+    "revision": "8e9280e8a6250c4a4130",
+    "url": "/kaustictest/static/js/2.2c77603f.chunk.js"
+  },
+  {
+    "revision": "5c67d2505568381bd155",
+    "url": "/kaustictest/static/css/main.f7dcc428.chunk.css"
+  },
+  {
+    "revision": "8f12c327a20f3183a66cf5a2099a54c4",
+    "url": "/kaustictest/index.html"
+  }
+];
